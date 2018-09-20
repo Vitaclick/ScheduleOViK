f82777aa3ef2b85bbea5ea464b835c85f7cd7dff
Vitaclick <vitaliy.zema@gmail.com>
2018-09-20 23:15:17 +0300

    Убран позор

--- Schedule/UpdateSchedule.cs
+++ Schedule/UpdateSchedule.cs
@@ -15,51 +15,47 @@ using Microsoft.Scripting.Hosting;
 
 namespace Schedule
 {
-    [Transaction(TransactionMode.Manual)]
-    public class UpdateSchedule : IExternalCommand
+  [Transaction(TransactionMode.Manual)]
+  public class UpdateSchedule : IExternalCommand
+  {
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+
+      // Get application and document objects
+      UIApplication ui_app = commandData.Application;
+      UIDocument ui_doc = ui_app?.ActiveUIDocument;
+      Document doc = ui_doc?.Document;
+      try
+      {
+
+        ScriptEngine engine = Python.CreateEngine();
+        ScriptScope scope = engine.CreateScope();
+        scope.SetVariable("doc", doc);
+        scope.SetVariable("uidoc", ui_doc);
+
+        string scriptName = Assembly.GetExecutingAssembly().GetName().Name + ".Resources." + "UpdateSchedule.py";
+        Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(scriptName);
+        if (stream != null)
         {
-
-            // Get application and document objects
-            UIApplication ui_app = commandData.Application;
-            UIDocument ui_doc = ui_app?.ActiveUIDocument;
-            Document doc = ui_doc?.Document;
-            try
-            {
-              Dictionary<string, object> options = new Dictionary<string, object> { ["Debug"] = true };
-
-               ScriptEngine engine = Python.CreateEngine(options);
-                ScriptScope scope = engine.CreateScope();
-                scope.SetVariable("doc", doc);
-                scope.SetVariable("uidoc", ui_doc);
-              ScriptSource script = engine.CreateScriptSourceFromFile(@"D:\#Projects\#REPOS\ScheduleOViK\Schedule\Resources\UpdateSchedule.py");
-
-
-        //                string scriptName = Assembly.GetExecutingAssembly().GetName().Name + ".Resources." + "UpdateSchedule.py";
-        //                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(scriptName);
-        //                if (stream != null)
-        //                {
-        //                    string script = new StreamReader(stream).ReadToEnd();
-        //                    engine.Execute(script, scope);
-        //                }
-        dynamic res = script.Execute(scope);
+          string script = new StreamReader(stream).ReadToEnd();
+          engine.Execute(script, scope);
+        }
 
         return Result.Succeeded;
-            }
-            // This is where we "catch" potential errors and define how to deal with them
-            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
-            {
-                // If user decided to cancel the operation return Result.Canceled
-                return Result.Cancelled;
-            }
-            catch (Exception ex)
-            {
-                // If something went wrong return Result.Failed
-                message = ex.Message;
-                return Result.Failed;
-            }
+      }
+      // This is where we "catch" potential errors and define how to deal with them
+      catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+      {
+        // If user decided to cancel the operation return Result.Canceled
+        return Result.Cancelled;
+      }
+      catch (Exception ex)
+      {
+        // If something went wrong return Result.Failed
+        message = ex.Message;
+        return Result.Failed;
+      }
 
-        }
     }
+  }
 }
